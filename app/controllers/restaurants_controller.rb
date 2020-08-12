6cb0180e074c2efb7a543e2e3770762bc2d488b6
Kristina Zhizherina <diablito.yey@mail.ru>
2020-08-12 15:39:34 +0200

    models complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,23 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get "restaurants", to: "restaurants#index"
+#  get "restaurants", to: "restaurants#index"
+#  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  resources :restaurants do
+    collection do
+       
+    end
+  end
+
 end
